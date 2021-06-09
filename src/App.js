00f101116ab1536caf131786d7365c002dd37524
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-06-09 18:24:06 -0300

    Exercicios de fixaçao React Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import StrictAccess from './components/StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+      <nav className="nav">
+        <ul>
+       <li> <Link to="/">Home</Link></li>
+       <li> <Link to="/about">About</Link></li>
+       <li> <Link to="/users">Users</Link></li>
+       <li> <Link to="/strict-access">Strict-Access</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+      <Route path="/about" component={ About } />
+      <Route path="/users/:id" render={(props) =>  <Users {...props} greetingsMessage="Good Morning" /> } />
+      <Route path="/strict-access" render={() => <StrictAccess  user = {{ username: "joao", password: "1234" }} /> } />
+      <Route path="/" component={ Home } />
+      </Switch>
+      </BrowserRouter>
     );
   }
 }
